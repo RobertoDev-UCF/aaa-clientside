e327f2f9fead50081e715627ff3290da330337ab
Joshua Townsend <jtownsend929@gmail.com>
2017-03-23 21:02:29 -0400

    added searching by member id to searchMember files

--- app/main/SearchMember/searchMember.controller.js
+++ app/main/SearchMember/searchMember.controller.js
@@ -10,6 +10,7 @@
             var vm = this;
 
             vm.SearchForMember = SearchForMember;
+			vm.SearchForMemberId = SearchForMemberId;
             vm.AddCustomer = AddCustomer;
 
             function AddCustomer() {
@@ -34,10 +35,32 @@
                 });
             }
             
+			function SearchForMemberId() {
+                if(!getMemberId() || getMemberId().length < 16) return;
+
+                var promise = $http({
+                    method: 'POST',
+                    url: "http://localhost:3050/GetCustomerByID",
+                    data: { customer_id: getMemberId() }
+                });
+
+                promise.then(function(result) {
+                    console.log(result);
+                    setMemberResults(result.data);
+                }).catch(function(err) {
+                    console.log(err);
+                    setMemberResults([]);
+                });
+            }
+			
             function getMemberNumber() {
                 return vm.InputMemberNumber;
             }
 
+			function getMemberId() {
+                return vm.InputMemberIdNumber;
+            }
+			
             function setMemberResults(value) {
                 vm.MemberResults = value;
             }
